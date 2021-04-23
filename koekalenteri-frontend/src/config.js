e0601c10f03187355c09ab28f49eaee79fd33ad0
Ramin Miraftabi <rmiraftabi@gmail.com>
2021-04-23 15:38:09 +0300

    Frontend config update

--- koekalenteri-frontend/src/config.js
+++ koekalenteri-frontend/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-    "aws_user_pools_web_client_id": "2u9u5g41791lmv8u78fic8ggpi",     // CognitoClientID
+    "aws_user_pools_web_client_id": "338fl248f87sphteskhtn0nnge",     // CognitoClientID
     "api_base_url": "",                                     // TodoFunctionApi
     "cognito_hosted_domain": "koekalenteri-koekalenteri-test.auth.eu-north-1.amazoncognito.com",                   // CognitoDomainName
-    "redirect_url": "https://master.d1tvh54pmhjni5.amplifyapp.com"                                      // AmplifyURL
+    "redirect_url": "https://master.d1405hsb64tm6b.amplifyapp.com"                                      // AmplifyURL
   };
   
   export default config;
