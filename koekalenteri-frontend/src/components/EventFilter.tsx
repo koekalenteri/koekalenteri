dc3ac11446e16fbc000e7ec6d1f2430e1af4c53e
kurkle <jukka.kurkela@gmail.com>
2021-09-20 18:42:38 +0300

    SC2: Unnecessary assertions

--- koekalenteri-frontend/src/components/EventFilter.tsx
+++ koekalenteri-frontend/src/components/EventFilter.tsx
@@ -23,7 +23,7 @@ export default function EventFilter({ filter, onChange }: EventFilterProps) {
           <Grid item xs={12} md={6}>
             <FormControl sx={{ mr: 1, width: '45%', minWidth: 150 }}>
               <InputLabel id="type-label">Koetyyppi</InputLabel>
-              <Select id="type" labelId="type-label" label={"Koetyyppi"} value={filter.eventType} onChange={(event) => setFilter({ eventType: event.target.value as string })}>
+              <Select id="type" labelId="type-label" label={"Koetyyppi"} value={filter.eventType} onChange={(event) => setFilter({ eventType: event.target.value })}>
                 <MenuItem value=''><em>Kaikki</em></MenuItem>
                 <MenuItem value='NOU'>NOU</MenuItem>
                 <MenuItem value='NOME-B'>NOME-B</MenuItem>
@@ -33,7 +33,7 @@ export default function EventFilter({ filter, onChange }: EventFilterProps) {
             </FormControl>
             <FormControl sx={{ width: '45%', minWidth: 150 }}>
               <InputLabel id="class-label">Koeluokka</InputLabel>
-              <Select id="type" labelId="class-label" label={"Koeluokka"} value={filter.eventClass} onChange={(event) => setFilter({ eventClass: event.target.value as string })}>
+              <Select id="type" labelId="class-label" label={"Koeluokka"} value={filter.eventClass} onChange={(event) => setFilter({ eventClass: event.target.value })}>
                 <MenuItem value=''><em>Kaikki</em></MenuItem>
                 <MenuItem value='ALO'>ALO</MenuItem>
                 <MenuItem value='AVO'>AVO</MenuItem>
