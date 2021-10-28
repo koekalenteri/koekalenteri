9c855942b48c18222cbb7339a0a33bd92565e312
kurkle <jukka.kurkela@gmail.com>
2021-10-29 00:34:44 +0300

    Fix test

--- koekalenteri-frontend/src/App.test.tsx
+++ koekalenteri-frontend/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders logo with proper ALT', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const imgElement = screen.getByAltText('Suomen noutajakoirajärjestö');
   expect(imgElement).toBeInTheDocument();
 });
