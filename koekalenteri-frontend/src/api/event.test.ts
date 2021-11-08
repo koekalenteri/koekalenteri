16e31a2375bf3c929eb065d2bd12b07969d3a972
kurkle <jukka.kurkela@gmail.com>
2021-11-08 19:09:36 +0200

    Proper event tests, revive dates from json

--- koekalenteri-frontend/src/api/event.test.ts
+++ koekalenteri-frontend/src/api/event.test.ts
@@ -1,13 +1,32 @@
+import fetchMock from "jest-fetch-mock";
 import { getEvents, getEvent } from './event';
+import { emptyEvent } from "koekalenteri-shared/src/test-utils/emptyEvent";
+import { API_BASE_URL } from "./http";
 
-jest.mock('./event');
+fetchMock.enableMocks();
+
+beforeEach(() => fetchMock.resetMocks());
 
 test('getEvents', async () => {
+  fetchMock.mockResponse(req => req.method === 'GET'
+    ? Promise.resolve(JSON.stringify([emptyEvent]))
+    : Promise.reject(new Error(`${req.method} !== 'GET'`)));
+
   const events = await getEvents();
-  expect(events.length).toEqual(5);
+
+  expect(events.length).toEqual(1);
+  expect(fetchMock.mock.calls.length).toEqual(1);
+  expect(fetchMock.mock.calls[0][0]).toEqual(API_BASE_URL + '/event/');
 });
 
 test('getEvent', async () => {
-  const event = await getEvent('type1', 'test1');
-  expect(event.id).toEqual('test1');
+  fetchMock.mockResponse(req => req.method === 'GET'
+    ? Promise.resolve(JSON.stringify(emptyEvent))
+    : Promise.reject(new Error(`${req.method} !== 'GET'`)));
+
+  const event = await getEvent('TestEventType', 'TestEventID');
+
+  expect(event).toEqual(emptyEvent);
+  expect(fetchMock.mock.calls.length).toEqual(1);
+  expect(fetchMock.mock.calls[0][0]).toEqual(API_BASE_URL + '/event/TestEventType/TestEventID');
 });
