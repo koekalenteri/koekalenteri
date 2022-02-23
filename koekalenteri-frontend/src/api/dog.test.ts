4e6295560b5e55aade4c13a4aff60aae7cfd0f42
kurkle <jukka.kurkela@gmail.com>
2022-02-23 22:47:14 +0200

    More FE coverage

--- koekalenteri-frontend/src/api/dog.test.ts
+++ koekalenteri-frontend/src/api/dog.test.ts
@@ -1,6 +1,12 @@
+import fetchMock from 'jest-fetch-mock';
+import { getDog } from './dog';
+import { API_BASE_URL } from "../config";
 import { Dog } from "koekalenteri-shared/model"
 import { rehydrateDog } from "./dog";
 
+fetchMock.enableMocks();
+
+beforeEach(() => fetchMock.resetMocks());
 
 test('rehydrateDog', function() {
   const dog: Dog = { regNo: 'abc', name: 'def', rfid: '', breedCode: '', dob: '20210101T00:00:00', refreshDate: '20200101T00:00:00' };
@@ -10,3 +16,21 @@ test('rehydrateDog', function() {
   expect(dog.dob).toBeInstanceOf(Date);
   expect(dog.refreshDate).toBeInstanceOf(Date);
 })
+
+test('getDog', async () => {
+  fetchMock.mockResponse(req => req.method === 'GET'
+    ? Promise.resolve(JSON.stringify({regNo: 'testReg'}))
+    : Promise.reject(new Error(`${req.method} !== 'GET'`)));
+
+  const dog = await getDog('testReg');
+
+  expect(dog.regNo).toEqual('testReg');
+  expect(fetchMock.mock.calls.length).toEqual(1);
+  expect(fetchMock.mock.calls[0][0]).toEqual(API_BASE_URL + '/dog/testReg');
+
+  const dog2 = await getDog('testReg2', true);
+
+  expect(dog2.regNo).toEqual('testReg');
+  expect(fetchMock.mock.calls.length).toEqual(2);
+  expect(fetchMock.mock.calls[1][0]).toEqual(API_BASE_URL + '/dog/testReg2?refresh');
+});
