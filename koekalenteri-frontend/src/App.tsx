a252b78b45a486c87c271ba33aa7cd8d32303d0c
kurkle <jukka.kurkela@gmail.com>
2022-03-05 22:52:16 +0200

    Install sizeme, update deps, update pages/index

--- koekalenteri-frontend/src/App.tsx
+++ koekalenteri-frontend/src/App.tsx
@@ -3,12 +3,11 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { SnackbarProvider } from 'notistack';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { locales, muiLocales, LocaleKey } from './i18n';
-import { SearchPage, EventRegistrationPage, EventListPage, JudgeListPage, UsersPage, OrganizationListPage } from './pages'
+import { EventEditPage, EventListPage, EventRegistrationPage, JudgeListPage, OrganizationListPage, SearchPage, UsersPage } from './pages'
 import { useTranslation } from 'react-i18next';
 import { makeStyles, ThemeProvider } from '@mui/styles';
 import { createTheme } from '@mui/material/styles';
 import { ADMIN_DEFAULT, ADMIN_EDIT_EVENT, ADMIN_EVENTS, ADMIN_JUDGES, ADMIN_NEW_EVENT, ADMIN_ORGS, ADMIN_ROOT, ADMIN_USERS } from './config';
-import { EventEditPage } from './pages/EventEditPage';
 
 const useStyles = makeStyles({
   snack: {
