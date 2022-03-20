aaaa7ab04b12e4d3645729647d5841bbe6d6ab2a
kurkle <jukka.kurkela@gmail.com>
2022-03-21 00:02:37 +0200

    KOE-112

---
+++ koekalenteri-backend/ses-templates/generate.mjs
@@ -0,0 +1,144 @@
+import Handlebars from 'handlebars';
+import remarkBreaks from 'remark-breaks'
+import remarkGfm from 'remark-gfm'
+import remarkHtml from 'remark-html'
+import remarkParse from 'remark-parse'
+import {readdirSync, writeFileSync} from 'fs'
+import {read} from 'to-vfile'
+import {reporter} from 'vfile-reporter'
+import {unified} from 'unified'
+import {visit, SKIP} from 'unist-util-visit'
+import tableHandler from './gen.tableHandler.mjs'
+
+const templates = readdirSync('.', {withFileTypes: true})
+  .filter(entry => entry.isDirectory() && entry.name !== 'samples')
+  .map(entry => entry.name);
+
+for (const template of templates) {
+  const context = await import(`./${template}/_sample.mjs`);
+
+  for (const lang of ['fi', 'en']) {
+    const templateObj = await genJson(template, lang);
+    writeFileSync(`${template}-${lang}.json`, JSON.stringify(templateObj));
+    writeFileSync(`samples/${template}-${lang}.txt`, Handlebars.compile(templateObj.Template.TextPart)(context.default));
+    writeFileSync(`samples/${template}-${lang}.html`, Handlebars.compile(templateObj.Template.HtmlPart)(context.default));
+  }
+}
+
+async function genJson(template, lang) {
+  const source = `${template}/${lang}.md`;
+
+  let subject = '';
+  const extractSubject = () =>
+    (tree) =>
+      visit(tree, (node, index, parent) => {
+        if (subject === '' && node.type === 'definition') {
+          subject = node.title;
+          parent.children.splice(index, 1);
+          return [SKIP, index];
+        }
+      });
+
+  const removeTableHead = () =>
+    (tree) =>
+      visit(tree, (node, index, parent) => {
+        if (node.type === 'tableRow' && index === 0 && !parent.headSkipped) {
+          parent.children.splice(index, 1);
+          parent.headSkipped = true;
+          return [SKIP, index];
+        }
+      });
+
+  const text = await unified()
+    .use(remarkParse)
+    .use(extractSubject)
+    .use(remarkGfm)
+    .use(removeTableHead)
+    .use(remarkPlainText)
+    .process(await read(source));
+
+  console.error(reporter(text))
+  Handlebars.precompile(String(text), {strict: true});
+
+  const html = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkBreaks)
+    .use(remarkHtml, {handlers: {table: tableHandler}})
+    .process(await read(source));
+
+  return {
+    "Template": {
+      "TemplateName": `${template}-${lang}`,
+      "SubjectPart": subject,
+      "TextPart": String(text),
+      "HtmlPart": String(html)
+    }
+  };
+}
+
+function remarkPlainText() {
+  Object.assign(this, {Compiler: compiler})
+
+  /**
+   * @type {import('unified').CompilerFunction<Root, string>}
+   */
+  function compiler(node, file) {
+    const result = toPlainText(node);
+
+    if (file.extname) {
+      file.extname = '.txt'
+    }
+
+    // Add an eof eol.
+    return node &&
+      node.type &&
+      node.type === 'root' &&
+      result &&
+      /[^\r\n]/.test(result.charAt(result.length - 1))
+      ? result + '\n'
+      : result
+  }
+}
+
+function toPlainText(node) {
+  return one(node);
+}
+
+function one(node) {
+  //console.warn(node);
+  return (
+    (node &&
+      typeof node === 'object' &&
+      ((node.value && formatValue(node)) ||
+        ('children' in node && all(node.type, node.children)) ||
+        (Array.isArray(node) && all('array', node)))) ||
+    ''
+  );
+}
+
+function formatValue(node) {
+  if (node.value.endsWith(':')) {
+    return node.value + ' ';
+  }
+  return node.value;
+}
+
+function all(type, values) {
+  /** @type {Array.<string>} */
+  var result = []
+  var index = -1
+
+  while (++index < values.length) {
+    result[index] = one(values[index])
+  }
+
+  if (type === 'tableRow' || type === 'table') {
+    result.push("\n");
+  }
+  if (type === 'paragraph' || type === 'heading') {
+    result.push("\n\n");
+  }
+
+  return result.join('')
+}
---
+++ koekalenteri-backend/src/utils/dates.ts
@@ -0,0 +1,31 @@
+import { isValid, isSameMonth, lightFormat, isSameDay, parseISO, isSameYear } from "date-fns";
+import { RegistrationTime } from "koekalenteri-shared/model";
+
+export function formatDateSpan(start: Date | string, end: Date | string): string {
+  if (typeof start === 'string') {
+    start = parseISO(start);
+  }
+  if (typeof end === 'string') {
+    end = parseISO(end);
+  }
+  if (!isValid(start)) {
+    return '';
+  }
+  if (!isValid(end)) {
+    end = start;
+  }
+  if (isSameDay(start, end)) {
+    return lightFormat(start, 'd.M.yyyy');
+  }
+  if (isSameMonth(start, end)) {
+    return lightFormat(start, 'd.') + '-' + lightFormat(end, 'd.M.yyyy');
+  }
+  if (isSameYear(start, end)) {
+    return lightFormat(start, 'd.M.') + '-' + lightFormat(end, 'd.M.yyyy');
+  }
+  return lightFormat(start, 'd.M.yyyy') + '-' + lightFormat(end, 'd.M.yyyy');
+}
+
+export function formatRegDate(date: string, time?: string) {
+  return lightFormat(parseISO(date), 'd.M.') + time ? ` ${time}` : '';
+}
