2fed5b61bb256d5302d407d126edfa016678507c
kurkle <jukka.kurkela@gmail.com>
2022-04-30 10:15:49 +0300

    Fix failing test when judge has no eventTypes

--- koekalenteri-frontend/src/stores/classes/CJudge.ts
+++ koekalenteri-frontend/src/stores/classes/CJudge.ts
@@ -32,6 +32,6 @@ export class CJudge {
     this.location = json.location;
     this.name = json.name;
     this.phone = json.phone;
-    this.search = [json.district, json.email, ...json.eventTypes, json.location, json.name, json.phone].map(v => v?.toLocaleLowerCase() || '').join(' ');
+    this.search = [json.district, json.email, ...(json.eventTypes || []), json.location, json.name, json.phone].map(v => v?.toLocaleLowerCase() || '').join(' ');
   }
 }
