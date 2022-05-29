959f73d5f31f30663d9c6aaed9e1f1ed591594ad
kurkle <jukka.kurkela@gmail.com>
2022-05-29 22:26:44 +0300

    Update SideMenu export/import

--- koekalenteri-frontend/src/pages/admin/AuthPage.tsx
+++ koekalenteri-frontend/src/pages/admin/AuthPage.tsx
@@ -3,8 +3,7 @@ import { Box, Toolbar } from '@mui/material';
 import { autorun } from 'mobx';
 import { ReactNode, useEffect, useState } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { Header } from '../../layout';
-import { SideMenu } from '../../layout/SideMenu';
+import { Header, SideMenu } from '../../layout';
 import { useSessionStarted, useStores } from '../../stores';
 
 export function AuthPage({ children, title }: { children: ReactNode, title?: string }) {
