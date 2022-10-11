44792a327be4522977c5c6331617375c8980fbee
Jukka Kurkela <jukka.kurkela@gmail.com>
2022-10-11 22:21:42 +0300

    feat: friendly name for secretary in email (#127)

--- koekalenteri-backend/src/utils/string.ts
+++ koekalenteri-backend/src/utils/string.ts
@@ -2,3 +2,8 @@
 export function capitalize(s: string) {
   return s.toLowerCase().replace(/(^|[ -])[^ -]/g, (l: string) => l.toUpperCase());
 }
+
+export function reverseName(name: string) {
+  const [last = '', first = ''] = name.split(' ');
+  return `${first} ${last}`.trim()
+}
