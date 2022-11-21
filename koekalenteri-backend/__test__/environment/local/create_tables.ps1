339439b7804ee000ed4d69d204602f262f1a5da7
Otto Esko <otto.esko@gmail.com>
2022-11-21 19:44:42 +0200

    KOE-355: Allow changing event type
    - Don't use event type in composite key of event table as it creates
      duplicate events if event type is changed

--- koekalenteri-backend/__test__/environment/local/create_tables.ps1
+++ koekalenteri-backend/__test__/environment/local/create_tables.ps1
@@ -3,8 +3,8 @@ $env:AWS_CLI_FILE_ENCODING="UTF-8"
 
 aws dynamodb create-table `
   --table-name event-table `
-  --attribute-definitions AttributeName=eventType,AttributeType=S AttributeName=id,AttributeType=S `
-  --key-schema AttributeName=eventType,KeyType=HASH AttributeName=id,KeyType=RANGE `
+  --attribute-definitions AttributeName=id,AttributeType=S `
+  --key-schema AttributeName=id,KeyType=HASH `
   --billing-mode PAY_PER_REQUEST --endpoint-url http://127.0.0.1:8000
 
 aws dynamodb batch-write-item --endpoint-url http://127.0.0.1:8000 --request-items file://events.json
