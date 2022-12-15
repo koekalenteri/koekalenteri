6c7329774c7f81274443dd683e8706db7e39038c
Jukka Kurkela <jukka.kurkela@gmail.com>
2022-12-15 22:32:49 +0200

    test: AdminHomePage

---
+++ koekalenteri-frontend/src/pages/admin/AdminHomePage.idle.test.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react'
+import { Authenticator } from '@aws-amplify/ui-react';
+import { AdminHomePage } from './AdminHomePage'
+import { ADMIN_ROOT } from '../../config';
+import { ThemeProvider } from '@mui/material';
+import theme from '../../assets/Theme';
+import { DataMemoryRouter, getHtml } from '../../test/utils';
+
+jest.mock('@aws-amplify/ui-react', () => require('./global-mocks/auth/idle'))
+
+describe('AdminHomePage', () => {
+  it('should redirect to login page if user is not logged in', () => {
+
+    const routes = [{
+      path: ADMIN_ROOT,
+      element: <AdminHomePage />
+    },{
+      path: '/login',
+      element: <>Login</>
+    }]
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Authenticator.Provider>
+          <DataMemoryRouter initialEntries={[ADMIN_ROOT]} routes={routes} />
+        </Authenticator.Provider>
+      </ThemeProvider>
+    )
+    expect(getHtml(container)).toMatchInlineSnapshot(`
+    "<div>
+      Login
+    </div>"
+    `)
+  })
+})
---
+++ koekalenteri-frontend/src/pages/admin/AdminHomePage.test.tsx
@@ -0,0 +1,46 @@
+import { render } from '@testing-library/react'
+import { Authenticator } from '@aws-amplify/ui-react';
+import { AdminHomePage } from './AdminHomePage'
+import { ADMIN_DEFAULT, ADMIN_ROOT } from '../../config';
+import { ThemeProvider } from '@mui/material';
+import theme from '../../assets/Theme';
+import { DataMemoryRouter, getHtml } from '../../test/utils';
+
+describe('AdminHomePage', () => {
+  it('renders the page when user is logged in', () => {
+    const routes = [{
+      path: ADMIN_ROOT,
+      element: <AdminHomePage />
+    },{
+      path: '/login',
+      element: <>Login</>
+    }]
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Authenticator.Provider>
+          <DataMemoryRouter initialEntries={[ADMIN_ROOT]} routes={routes} />
+        </Authenticator.Provider>
+      </ThemeProvider>
+    )
+    expect(getHtml(container)).toMatchSnapshot()
+  })
+
+  it('renders the child page content when user is logged in', () => {
+    const routes = [{
+      path: ADMIN_ROOT,
+      element: <AdminHomePage />,
+      children: [{
+        path: ADMIN_DEFAULT,
+        element: <>ADMIN DEFAULT PAGE CONTENT</>
+      }]
+    }]
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Authenticator.Provider>
+          <DataMemoryRouter initialEntries={[ADMIN_DEFAULT]} routes={routes} />
+        </Authenticator.Provider>
+      </ThemeProvider>
+    )
+    expect(getHtml(container)).toMatchSnapshot()
+  })
+})
