1c679616ce801c46cd716add33f7189548b0b894
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-05 03:03:17 +0200

    fix: AdditionalInfoSection smells

--- koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/AdditionalInfoSection.tsx
+++ koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/AdditionalInfoSection.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
 import { TextField } from '@mui/material'
 
 import { CollapsibleSection } from '../../../../components'
@@ -5,9 +7,15 @@ import { SectionProps } from '../EventForm'
 
 
 export default function AdditionalInfoSection({ event, onChange, onOpenChange, open }: SectionProps) {
+  const { t } = useTranslation()
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    onChange({ description: e.target.value })
+  }, [onChange])
+
   return (
-    <CollapsibleSection title="Lisätiedot" open={open} onOpenChange={onOpenChange}>
-      <TextField rows={5} fullWidth multiline value={event.description} onChange={(e) => onChange({ description: e.target.value })}></TextField>
+    <CollapsibleSection title={t('event.description')} open={open} onOpenChange={onOpenChange}>
+      <TextField rows={5} fullWidth multiline value={event.description} onChange={handleChange}></TextField>
     </CollapsibleSection>
   )
 }
