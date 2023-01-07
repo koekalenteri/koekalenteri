37508c189b5d4e6ed45740c496b3e8b7c039f11d
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-07 11:27:28 +0200

    fix: EventPrice smells

--- koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/components/EventPrice.tsx
+++ koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/components/EventPrice.tsx
@@ -1,16 +1,22 @@
+import { useCallback } from "react"
 import { InputAdornment } from "@mui/material"
+import { Event } from "koekalenteri-shared/model"
 
 import EventProperty, { EventPropertyProps } from "./EventProperty"
 
-type ValidatedPriceInputProps = Omit<EventPropertyProps<'cost' | 'costMember', true>, 'options'>;
-export default function EventPrice(props: ValidatedPriceInputProps) {
+type Props = Omit<EventPropertyProps<'cost' | 'costMember', true>, 'options'>
+
+export default function EventPrice(props: Props) {
+  const toString = useCallback((v?: string|number) => v?.toString() ?? '', [])
+  const handleChange = useCallback((newProps: Partial<Event>) => props.onChange({ [props.id]: +(newProps[props.id] || '') }), [props])
   return (
     <EventProperty
       {...props}
       freeSolo
       options={[30, 35, 40, 45]}
-      getOptionLabel={(v) => v?.toString() || ''}
+      getOptionLabel={toString}
       endAdornment={<InputAdornment position="end">€</InputAdornment>}
-      onChange={(newProps) => props.onChange({ [props.id]: +(newProps[props.id] || '') })} />
+      onChange={handleChange}
+    />
   )
 }
