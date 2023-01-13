082f5d5134d9610c43d4e1d32025e6e3e02f7d38
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-13 16:42:24 +0200

    feat: test utils

--- koekalenteri-frontend/src/utils.ts
+++ koekalenteri-frontend/src/utils.ts
@@ -1,5 +1,5 @@
-import { endOfDay, startOfDay, subDays } from 'date-fns'
-import type { Event, JsonValue } from 'koekalenteri-shared/model'
+import { eachDayOfInterval, endOfDay, startOfDay, subDays } from 'date-fns'
+import { Event, JsonValue, RegistrationDate } from 'koekalenteri-shared/model'
 
 type EventDates = {
   startDate?: Date
@@ -18,8 +18,10 @@ export const isEntryClosed = ({ startDate, entryEndDate }: EventDates, now = new
 export const isEventOngoing = ({ startDate, endDate }: EventDates, now = new Date()) => !!startDate && !!endDate && startDate <= now && endDate >= now
 export const isEventOver = ({ startDate }: EventDates, now = new Date()) => !!startDate && startDate < now
 
+export const eventDates = (event: Event) => event.classes.length ? uniqueDate(event.classes.map(c => c.date ?? event.startDate)) : eachDayOfInterval({ start: event.startDate, end: event.endDate })
 export const uniqueClasses = (event?: Event) => unique((event?.classes ?? []).map(c => c.class))
-export const uniqueClassDates = (event: Event, cls: string) => uniqueDate(event.classes.filter(c => c.class === cls).map(c => c.date || event.startDate || new Date()))
+export const uniqueClassDates = (event: Event, cls: string) => uniqueDate(event.classes.filter(c => c.class === cls).map(c => c.date ?? event.startDate))
+export const registrationDates = (event: Event, cls?: string) => (cls ? uniqueClassDates(event, cls) : eventDates(event)).flatMap<RegistrationDate>(date => [{ date, time: 'ap' }, { date, time: 'ip' }])
 
 export function entryDateColor(event: Event) {
   if (!isEntryOpen(event)) {
@@ -28,17 +30,9 @@ export function entryDateColor(event: Event) {
   return isEntryClosing(event) ? 'warning.main' : 'success.main'
 }
 
-export function unique<T = string>(arr: T[]): T[] {
-  return arr.filter((c, i, a) => a.indexOf(c) === i)
-}
-
-export function uniqueFn<T>(arr: T[], cmp: (a: T, b: T) => boolean): T[] {
-  return arr.filter((c, i, a) => a.findIndex(f => cmp(f, c)) === i)
-}
-
-export function uniqueDate(arr: Date[]): Date[] {
-  return uniqueFn<Date>(arr, (a, b) => a.valueOf() === b.valueOf())
-}
+export const unique = <T = string>(arr: T[]): T[] => arr.filter((c, i, a) => a.indexOf(c) === i)
+export const uniqueFn = <T>(arr: T[], cmp: (a: T, b: T) => boolean): T[] => arr.filter((c, i, a) => a.findIndex(f => cmp(f, c)) === i)
+export const uniqueDate = (arr: Date[]) => [...new Set<number>(arr.map(d => d.valueOf()))].map(v => new Date(v))
 
 function dateReviver(_key: string, value: JsonValue): JsonValue | Date {
   if (typeof value === 'string' && /^\d{4}-[01]\d-[0-3]\dT[012]\d(?::[0-6]\d){2}\.\d{3}Z$/.test(value)) {
