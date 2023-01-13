d3697bccd6788bfa60456fb129d769f28ec79f7f
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-13 17:59:30 +0200

    fix: dateReviver for short iso format

--- koekalenteri-frontend/src/utils.test.ts
+++ koekalenteri-frontend/src/utils.test.ts
@@ -21,6 +21,7 @@ describe('utils', () => {
       expect(parseJSON('{"pvm":"2021-05-10T09:05:12.000Z"}')).toEqual({
         pvm: new Date("2021-05-10T09:05:12.000Z"),
       })
+      expect(parseJSON('{"date":"2021-05-10T00:00:00"}').date).toBeInstanceOf(Date)
     })
   })
 
--- koekalenteri-frontend/src/utils.ts
+++ koekalenteri-frontend/src/utils.ts
@@ -35,7 +35,7 @@ export const uniqueFn = <T>(arr: T[], cmp: (a: T, b: T) => boolean): T[] => arr.
 export const uniqueDate = (arr: Date[]) => [...new Set<number>(arr.map(d => d.valueOf()))].map(v => new Date(v))
 
 function dateReviver(_key: string, value: JsonValue): JsonValue | Date {
-  if (typeof value === 'string' && /^\d{4}-[01]\d-[0-3]\dT[012]\d(?::[0-6]\d){2}\.\d{3}Z$/.test(value)) {
+  if (typeof value === 'string' && /^\d{4}-(?:0[1-9]|1[0-2])-(?:[0-2][1-9]|[1-3]0|3[01])T(?:[0-1][0-9]|2[0-3])(?::[0-6]\d)(?::[0-6]\d)?(?:\.\d{3})?(?:[+-][0-2]\d:[0-5]\d|Z)?$/.test(value)) {
     const date = new Date(value)
     if (!isNaN(+date)) {
       return date
