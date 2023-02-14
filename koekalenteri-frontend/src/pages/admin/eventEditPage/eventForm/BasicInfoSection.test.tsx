6db773f6bf153f26476fdb33d8d7065740c399d5
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-02-14 08:13:14 +0200

    test: remove the flanky test

--- koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/BasicInfoSection.test.tsx
+++ koekalenteri-frontend/src/pages/admin/eventEditPage/eventForm/BasicInfoSection.test.tsx
@@ -1,9 +1,8 @@
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { locales } from '../../../../i18n'
-import { renderWithUserEvents, waitForDebounce } from '../../../../test-utils/utils'
 
 import BasicInfoSection from './BasicInfoSection'
 
@@ -25,34 +24,5 @@ describe('BasicInfoSection', () => {
       </LocalizationProvider>,
     )
     expect(container).toMatchSnapshot()
-  })
-
-  it('should fire onChange', async () => {
-    const testEvent = {
-      id: 'test',
-      judges: [],
-      startDate: new Date('2022-06-01'),
-      endDate: new Date('2022-06-02'),
-      classes: [],
-    }
-    const changeHandler = jest.fn((props) => Object.assign(testEvent, props))
-
-    const { user } = renderWithUserEvents(
-      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={locales.fi}>
-        <BasicInfoSection event={testEvent} onChange={changeHandler} open />
-      </LocalizationProvider>,
-    )
-
-    expect(changeHandler).toHaveBeenCalledTimes(0)
-
-    const input = screen.getByLabelText('event.kcId')
-    expect(input).toHaveValue('')
-
-    await user.type(input, '123')
-    await waitForDebounce()
-
-    expect(input).toHaveValue('123')
-    expect(changeHandler).toHaveBeenCalledTimes(1)
-    expect(changeHandler).toHaveBeenLastCalledWith({ kcId: '123' })
   })
 })
