52c7126c1c2174d202374bcfcba7b7e8041f0140
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-03-21 08:09:18 +0200

    fix: email preview dataa

--- koekalenteri-frontend/src/i18n/dates.ts
+++ koekalenteri-frontend/src/i18n/dates.ts
@@ -13,15 +13,18 @@ import { Language } from 'koekalenteri-shared/model'
 
 export const locales: Record<Language, Locale> = { en, fi }
 
-export const formatDate =
-  (fmt: string) =>
-  (date: Date | string, lng: string | undefined): string => {
-    const locale = locales[lng as Language]
-    if (typeof date === 'string') {
-      date = parseISO(date)
-    }
-    return format(date, fmt, { locale })
+export const formatDate = (date: Date | string, fmt: string, lng?: string): string => {
+  const locale = locales[lng as Language]
+  if (typeof date === 'string') {
+    date = parseISO(date)
   }
+  return format(date, fmt, { locale })
+}
+
+export const getDateFormatter =
+  (fmt: string) =>
+  (date: Date | string, lng: string | undefined): string =>
+    formatDate(date, fmt, lng)
 
 export function formatDateSpan(start: Date | string, lng: string | undefined, { end }: { end: Date | string }): string {
   if (typeof start === 'string') {
