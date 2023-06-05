6c413cd59da32557683dafbd5b6818746cea44ac
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-06-05 21:34:24 +0300

    fix: registrations need to be sorted  by class

--- koekalenteri-backend/src/handlers/admin/registration.ts
+++ koekalenteri-backend/src/handlers/admin/registration.ts
@@ -39,13 +39,15 @@ const numberGroupKey = <T extends JsonRegistration>(reg: T) => {
   return 'reserve-' + ct
 }
 
-const byKeyAndNumber = <T extends JsonRegistration>(a: T, b: T): number =>
-  a.group?.key === b.group?.key
-    ? (a.group?.number || 999) - (b.group?.number || 999)
-    : (a.group?.key ?? '').localeCompare(b.group?.key ?? '')
+const byClassKeyAndNumber = <T extends JsonRegistration>(a: T, b: T): number =>
+  a.class === b.class
+    ? a.group?.key === b.group?.key
+      ? (a.group?.number || 999) - (b.group?.number || 999)
+      : (a.group?.key ?? '').localeCompare(b.group?.key ?? '')
+    : (a.class ?? '').localeCompare(b.class ?? '')
 
 export async function fixGroups<T extends JsonRegistration>(items: T[]): Promise<T[]> {
-  items.sort(byKeyAndNumber)
+  items.sort(byClassKeyAndNumber)
 
   const grouped: Record<string, T[]> = {}
   for (const item of items) {
