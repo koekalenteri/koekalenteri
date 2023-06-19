b0f5b7aa26963dcd10ef11e96cca12db14cb83d5
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-06-19 08:28:47 +0300

    feat: registration confirmed status indicator

--- koekalenteri-frontend/src/pages/admin/eventViewPage/classEntrySelection/useClassEntrySectionColumns.tsx
+++ koekalenteri-frontend/src/pages/admin/eventViewPage/classEntrySelection/useClassEntrySectionColumns.tsx
@@ -1,9 +1,12 @@
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
+import CheckOutlined from '@mui/icons-material/CheckOutlined'
 import DragIndicatorOutlined from '@mui/icons-material/DragIndicatorOutlined'
 import EditOutlined from '@mui/icons-material/EditOutlined'
 import EuroOutlined from '@mui/icons-material/EuroOutlined'
 import EventBusyOutlined from '@mui/icons-material/EventBusyOutlined'
 import PersonOutline from '@mui/icons-material/PersonOutline'
+import Tooltip from '@mui/material/Tooltip'
 import { GridActionsCellItem, GridColumns, GridValueGetterParams } from '@mui/x-data-grid'
 import { BreedCode, Registration } from 'koekalenteri-shared/model'
 
@@ -12,117 +15,130 @@ import GroupColors from './GroupColors'
 export function useClassEntrySelectionColumns(eventDates: Date[], openEditDialog?: (id: string) => void) {
   const { t } = useTranslation()
 
-  const entryColumns: GridColumns<Registration> = [
-    {
-      cellClassName: 'nopad',
-      field: 'dates',
-      headerName: '',
-      width: 56,
-      minWidth: 56,
-      renderCell: (p) => (
-        <>
-          <DragIndicatorOutlined />
-          <GroupColors dates={eventDates} selected={p.row.dates} />
-        </>
-      ),
-      sortable: false,
-    },
-    {
-      align: 'right',
-      cellClassName: 'nopad',
-      field: 'number',
-      headerAlign: 'right',
-      headerClassName: 'nopad',
-      headerName: '#',
-      width: 20,
-      minWidth: 20,
-      sortable: false,
-      valueGetter: (p) => (p.row.group?.number ? `${p.row.group.number}.` : ''),
-    },
-    {
-      field: 'dog.name',
-      headerName: t('dog.name'),
-      width: 250,
-      flex: 1,
-      sortable: false,
-      valueGetter: (p: GridValueGetterParams<string, Registration>) => p.row.dog.name,
-    },
-    {
-      field: 'dog.regNo',
-      headerName: t('dog.regNo'),
-      width: 130,
-      sortable: false,
-      valueGetter: (p) => p.row.dog.regNo,
-    },
-    {
-      field: 'dob.breed',
-      headerName: t('dog.breed'),
-      width: 150,
-      sortable: false,
-      valueGetter: (p: GridValueGetterParams<BreedCode, Registration>) =>
-        p.row.dog?.breedCode && p.row.dog?.gender
-          ? t(`${p.row.dog.breedCode}.${p.row.dog.gender}`, { ns: 'breedAbbr', defaultValue: p.row.dog.breedCode })
-          : '',
-    },
-    {
-      field: 'handler',
-      headerName: t('registration.handler'),
-      width: 150,
-      flex: 1,
-      sortable: false,
-      valueGetter: (p) => p.row.handler.name,
-    },
-    {
-      field: 'member',
-      headerName: t('registration.member'),
-      width: 60,
-      align: 'center',
-      renderCell: (p) => (p.row.handler.membership ? <PersonOutline fontSize="small" /> : <></>),
-      sortable: false,
-    },
-    {
-      field: 'paid',
-      headerName: t('registration.paid'),
-      width: 90,
-      align: 'center',
-      renderCell: () => <EuroOutlined fontSize="small" />,
-      sortable: false,
-    },
-    {
-      cellClassName: 'nopad',
-      field: 'actions',
-      type: 'actions',
-      headerName: '',
-      width: 30,
-      minWidth: 30,
-      sortable: false,
-      getActions: (p) => [
-        <GridActionsCellItem
-          icon={<EditOutlined fontSize="small" />}
-          label={t('edit')}
-          onClick={() => openEditDialog?.(p.row.id)}
-          showInMenu
-        />,
-        <GridActionsCellItem
-          icon={<EventBusyOutlined fontSize="small" />}
-          label={t('withdraw')}
-          onClick={() => openEditDialog?.(p.row.id)}
-          showInMenu
-        />,
-      ],
-    },
-  ]
+  return useMemo(() => {
+    const entryColumns: GridColumns<Registration> = [
+      {
+        cellClassName: 'nopad',
+        field: 'dates',
+        headerName: '',
+        width: 56,
+        minWidth: 56,
+        renderCell: (p) => (
+          <>
+            <DragIndicatorOutlined />
+            <GroupColors dates={eventDates} selected={p.row.dates} />
+          </>
+        ),
+        sortable: false,
+      },
+      {
+        align: 'right',
+        cellClassName: 'nopad',
+        field: 'number',
+        headerAlign: 'right',
+        headerClassName: 'nopad',
+        headerName: '#',
+        width: 20,
+        minWidth: 20,
+        sortable: false,
+        valueGetter: (p) => (p.row.group?.number ? `${p.row.group.number}.` : ''),
+      },
+      {
+        field: 'dog.name',
+        headerName: t('dog.name'),
+        width: 250,
+        flex: 1,
+        sortable: false,
+        valueGetter: (p: GridValueGetterParams<string, Registration>) => p.row.dog.name,
+      },
+      {
+        field: 'dog.regNo',
+        headerName: t('dog.regNo'),
+        width: 130,
+        sortable: false,
+        valueGetter: (p) => p.row.dog.regNo,
+      },
+      {
+        field: 'dob.breed',
+        headerName: t('dog.breed'),
+        width: 150,
+        sortable: false,
+        valueGetter: (p: GridValueGetterParams<BreedCode, Registration>) =>
+          p.row.dog?.breedCode && p.row.dog?.gender
+            ? t(`${p.row.dog.breedCode}.${p.row.dog.gender}`, { ns: 'breedAbbr', defaultValue: p.row.dog.breedCode })
+            : '',
+      },
+      {
+        field: 'handler',
+        headerName: t('registration.handler'),
+        width: 150,
+        flex: 1,
+        sortable: false,
+        valueGetter: (p) => p.row.handler.name,
+      },
+      {
+        field: 'icons',
+        headerName: '',
+        width: 90,
+        align: 'center',
+        renderCell: (p) => (
+          <>
+            <Tooltip
+              title={`Ilmoittautuja ${p.row.handler.membership ? 'on' : 'ei ole'} järjestävän yhdistyksen jäsen`}
+              placement="left"
+            >
+              <PersonOutline fontSize="small" sx={{ opacity: p.row.handler.membership ? 1 : 0.05 }} />
+            </Tooltip>
+            <Tooltip
+              title={`Ilmoittautuja ${p.row.paidAt ? 'on' : 'ei ole'} järjestävän yhdistyksen jäsen`}
+              placement="left"
+            >
+              <EuroOutlined fontSize="small" sx={{ opacity: p.row.paidAt ? 1 : 0.05 }} />
+            </Tooltip>
+            <Tooltip
+              title={`Ilmoittautuja ${p.row.confirmed ? 'on' : 'ei ole'} vahvistanut ottavansa vastaan koepaikan`}
+              placement="left"
+            >
+              <CheckOutlined fontSize="small" sx={{ opacity: p.row.confirmed ? 1 : 0.05 }} />
+            </Tooltip>
+          </>
+        ),
+        sortable: false,
+      },
+      {
+        cellClassName: 'nopad',
+        field: 'actions',
+        type: 'actions',
+        headerName: '',
+        width: 30,
+        minWidth: 30,
+        sortable: false,
+        getActions: (p) => [
+          <GridActionsCellItem
+            icon={<EditOutlined fontSize="small" />}
+            label={t('edit')}
+            onClick={() => openEditDialog?.(p.row.id)}
+            showInMenu
+          />,
+          <GridActionsCellItem
+            icon={<EventBusyOutlined fontSize="small" />}
+            label={t('withdraw')}
+            onClick={() => openEditDialog?.(p.row.id)}
+            showInMenu
+          />,
+        ],
+      },
+    ]
 
-  const participantColumns: GridColumns<Registration> = [...entryColumns]
+    const participantColumns: GridColumns<Registration> = [...entryColumns]
 
-  const cancelledColumns: GridColumns<Registration> = [
-    ...participantColumns,
-    {
+    const cancelledColumns: GridColumns<Registration> = [...participantColumns]
+    cancelledColumns.splice(cancelledColumns.length - 2, 0, {
       field: 'cancelReason',
       headerName: 'Perumisen syy',
       width: 90,
-    },
-  ]
+    })
 
-  return { cancelledColumns, entryColumns, participantColumns }
+    return { cancelledColumns, entryColumns, participantColumns }
+  }, [eventDates, openEditDialog, t])
 }
