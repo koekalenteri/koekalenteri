c4c64cbcf5a8204c10f57f434c864c5fa5766ed4
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-06-19 21:49:16 +0300

    fix: each child warning

--- koekalenteri-frontend/src/pages/components/PriorityChips.tsx
+++ koekalenteri-frontend/src/pages/components/PriorityChips.tsx
@@ -10,7 +10,7 @@ export const PriorityChips = ({ priority }: Props) => (
   <>
     {priority.map((value) => {
       const p = PRIORITY.find((p) => p.value === value)
-      return p ? <Chip label={p.name} size="small" /> : null
+      return p ? <Chip key={p.value} label={p.name} size="small" /> : null
     })}
   </>
 )
