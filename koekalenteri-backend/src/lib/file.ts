66042a6cc832d22651f29cb32a3e5715d05f3164
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-08-03 11:56:53 +0300

    chore: add exception handling / logging

--- koekalenteri-backend/src/lib/file.ts
+++ koekalenteri-backend/src/lib/file.ts
@@ -18,60 +18,86 @@ interface ParseResult {
 
 export const parsePostFile = (event: APIGatewayProxyEvent) =>
   new Promise<ParseResult>((resolve, reject) => {
-    const bb = busboy({
-      headers: {
-        'content-type': event.headers['content-type'] ?? event.headers['Content-Type'],
-      },
-      limits: {
-        fileSize: 20e6, // 20 MB
-      },
-    })
+    try {
+      console.log('Parsing File from event with headers: ', event.headers)
+      const bb = busboy({
+        headers: {
+          'content-type': event.headers['content-type'] ?? event.headers['Content-Type'],
+        },
+        limits: {
+          fileSize: 20e6, // 20 MB
+        },
+      })
 
-    const result: ParseResult = {
-      fields: {},
-      info: undefined,
-      data: '',
-      error: null,
+      const result: ParseResult = {
+        fields: {},
+        info: undefined,
+        data: '',
+        error: null,
+      }
+
+      bb.on('file', (_name: string, file: Readable, info: FileInfo): void => {
+        file.on('data', (data) => {
+          result.data = data
+        })
+
+        file.on('end', () => {
+          result.error = file.errored
+          result.info = info
+        })
+      })
+
+      bb.on('field', (name, value) => {
+        result.fields[name] = value
+      })
+
+      bb.on('error', (error) => {
+        console.error(error)
+        reject(error)
+      })
+      bb.on('finish', () => {
+        resolve(result)
+      })
+    } catch (e) {
+      console.error(e)
+      reject(e)
     }
-
-    bb.on('file', (_name: string, file: Readable, info: FileInfo): void => {
-      file.on('data', (data) => {
-        result.data = data
-      })
-
-      file.on('end', () => {
-        result.error = file.errored
-        result.info = info
-      })
-    })
-
-    bb.on('field', (name, value) => {
-      result.fields[name] = value
-    })
-
-    bb.on('error', (error) => reject(error))
-    bb.on('finish', () => {
-      resolve(result)
-    })
   })
 
 export const uploadFile = (key: string, buffer: S3.Body) =>
   new Promise<void>((resolve, reject) => {
-    const data: S3.Types.PutObjectRequest = {
-      Bucket: process.env.BUCKET ?? '',
-      Key: key,
-      Body: buffer,
+    try {
+      console.log(`Uploading file to S3 bucket "${process.env.BUCKET ?? ''}" with key "${key}"`)
+      const data: S3.Types.PutObjectRequest = {
+        Bucket: process.env.BUCKET ?? '',
+        Key: key,
+        Body: buffer,
+      }
+      s3.putObject(data, (error) => {
+        if (error) {
+          console.error(error)
+          return reject(error)
+        }
+        resolve()
+      })
+    } catch (e) {
+      console.error(e)
+      reject(e)
     }
-    s3.putObject(data, (error) => {
-      if (error) return reject(error)
-      resolve()
-    })
   })
 
 export const downloadFile = (key: string) =>
   new Promise<GetObjectOutput>((resolve, reject) => {
-    s3.getObject({ Bucket: process.env.BUCKET ?? '', Key: key }, (error, data) => {
-      if (error) return reject(error)
-      resolve(data)
-    })
+    console.log(`Downloading file from S3 bucket "${process.env.BUCKET ?? ''}" with key "${key}"`)
+    try {
+      s3.getObject({ Bucket: process.env.BUCKET ?? '', Key: key }, (error, data) => {
+        if (error) {
+          console.error(error)
+          return reject(error)
+        }
+        resolve(data)
+      })
+    } catch (error) {
+      reject(error)
+    }
   })
