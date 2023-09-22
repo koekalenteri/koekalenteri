10085f8239a5087cc5133b522c3dc69c567bc046
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-09-22 11:33:22 +0300

    feat: putOrganizer

--- koekalenteri-backend/src/handlers/admin/organizer.ts
+++ koekalenteri-backend/src/handlers/admin/organizer.ts
@@ -37,36 +37,80 @@ export const getOrganizersHandler = metricScope(
     }
 )
 
-export const refreshOrganizers = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const user = await authorize(event)
-  if (!user?.admin) {
-    return response(401, 'Unauthorized', event)
-  }
-
-  const { status, json } = await klapi.lueYhdistykset({ Rajaus: KLYhdistysRajaus.Koejärjestätä })
-  if (status === 200 && json) {
-    const existing = await dynamoDB.readAll<Organizer>()
-    for (const item of json) {
-      const old = existing?.find((org) => org.kcId === item.jäsennumero)
-      if (!old) {
-        const org: Organizer = { id: nanoid(10), kcId: item.jäsennumero, name: item.strYhdistys }
-        await dynamoDB.write(org)
-      } else {
-        if (old.name !== item.strYhdistys) {
-          await dynamoDB.update(
-            { id: old.id },
-            'set #name = :name',
-            {
-              '#name': 'name',
-            },
-            {
-              ':name': item.strYhdistys,
-            }
-          )
+export const refreshOrganizers = metricScope(
+  (metrics: MetricsLogger) =>
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+      try {
+        const user = await authorize(event)
+        if (!user?.admin) {
+          metricsError(metrics, event.requestContext, 'refreshOrganizers')
+          return response(401, 'Unauthorized', event)
         }
+
+        const { status, json } = await klapi.lueYhdistykset({ Rajaus: KLYhdistysRajaus.Koejärjestätä })
+        if (status === 200 && json) {
+          const existing = await dynamoDB.readAll<Organizer>()
+          for (const item of json) {
+            const old = existing?.find((org) => org.kcId === item.jäsennumero)
+            if (!old) {
+              const org: Organizer = { id: nanoid(10), kcId: item.jäsennumero, name: item.strYhdistys }
+              await dynamoDB.write(org)
+            } else {
+              if (old.name !== item.strYhdistys) {
+                await dynamoDB.update(
+                  { id: old.id },
+                  'set #name = :name',
+                  {
+                    '#name': 'name',
+                  },
+                  {
+                    ':name': item.strYhdistys,
+                  }
+                )
+              }
+            }
+          }
+        }
+        const items = await dynamoDB.readAll<Organizer>()
+
+        metricsSuccess(metrics, event.requestContext, 'refreshOrganizers')
+        return response(200, items, event)
+      } catch (err) {
+        console.error(err)
+        metricsError(metrics, event.requestContext, 'refreshOrganizers')
+        return response((err as AWSError).statusCode || 501, err, event)
       }
     }
-  }
-  const items = await dynamoDB.readAll<Organizer>()
-  return response(200, items, event)
-}
+)
+
+export const putOrganizer = metricScope(
+  (metrics: MetricsLogger) =>
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+      try {
+        const user = await authorize(event)
+        if (!user?.admin) {
+          metricsError(metrics, event.requestContext, 'putOrganizer')
+          return response(401, 'Unauthorized', event)
+        }
+
+        const item: Partial<Organizer> = JSON.parse(event.body || '{}')
+
+        if (!item.id) {
+          metricsError(metrics, event.requestContext, 'putOrganizer')
+          return response(400, 'no data', event)
+        }
+
+        const existing = await dynamoDB.read<Organizer>({ id: item.id })
+        const updated = { ...existing, ...item }
+
+        await dynamoDB.write(updated)
+
+        metricsSuccess(metrics, event.requestContext, 'putOrganizer')
+        return response(200, updated, event)
+      } catch (err) {
+        console.error(err)
+        metricsError(metrics, event.requestContext, 'putOrganizer')
+        return response((err as AWSError).statusCode || 501, err, event)
+      }
+    }
+)
