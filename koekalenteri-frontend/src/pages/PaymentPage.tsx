da6d3162ecf61f51f559f5e01b2459da0a1398cf
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-10-14 15:22:25 +0300

    fix: payment page loading

--- koekalenteri-frontend/src/pages/PaymentPage.tsx
+++ koekalenteri-frontend/src/pages/PaymentPage.tsx
@@ -1,7 +1,8 @@
 import type { CreatePaymentResponse } from 'koekalenteri-shared/model'
 import type { Params } from 'react-router-dom'
 
-import { useLoaderData, useParams } from 'react-router-dom'
+import { Suspense } from 'react'
+import { Await, defer, useLoaderData, useParams } from 'react-router-dom'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
@@ -10,16 +11,21 @@ import { useRecoilValue } from 'recoil'
 import { createPayment } from '../api/payment'
 
 import { ProviderButton } from './paymentPage/ProviderButton'
+import { LoadingPage } from './LoadingPage'
 import { confirmedEventSelector, registrationSelector } from './recoil'
 
 export const paymentLoader = async ({ params }: { params: Params<string> }) =>
-  params.id && params.registrationId ? createPayment(params.id, params.registrationId) : {}
+  params.id && params.registrationId ? defer({ response: createPayment(params.id, params.registrationId) }) : {}
 
-export const PaymentPage = () => {
-  const { id, registrationId } = useParams()
+interface Props {
+  id?: string
+  registrationId?: string
+  response?: CreatePaymentResponse
+}
+
+const PaymentPageWithData = ({ id, registrationId, response }: Props) => {
   const event = useRecoilValue(confirmedEventSelector(id))
   const registration = useRecoilValue(registrationSelector(`${id ?? ''}:${registrationId ?? ''}`))
-  const response = useLoaderData() as CreatePaymentResponse
 
   if (!event || !registration) {
     return <>Tapahtumaa {id} ei löydy.</>
@@ -37,7 +43,7 @@ export const PaymentPage = () => {
       </Typography>
 
       {response.groups.map((group) => (
-        <Paper sx={{ p: 1, m: 1 }} elevation={0}>
+        <Paper key={group.id} sx={{ p: 1, m: 1 }} elevation={0}>
           <Typography variant="h6">{group.name}</Typography>
           <Grid container spacing={1}>
             {response.providers
@@ -51,5 +57,18 @@ export const PaymentPage = () => {
         </Paper>
       ))}
     </Paper>
+  )
+}
+
+export const PaymentPage = () => {
+  const { id, registrationId } = useParams()
+  const data = useLoaderData() as { response: CreatePaymentResponse }
+
+  return (
+    <Suspense fallback={<LoadingPage />}>
+      <Await resolve={data.response}>
+        {(response) => <PaymentPageWithData id={id} registrationId={registrationId} response={response} />}
+      </Await>
+    </Suspense>
   )
 }
