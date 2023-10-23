8c6898f59b6cbe271f5f2a310c8e7c8f422d6f3e
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-10-23 23:42:39 +0300

    fix: officials effect

--- koekalenteri-frontend/src/pages/admin/recoil/officials/effects.ts
+++ koekalenteri-frontend/src/pages/admin/recoil/officials/effects.ts
@@ -4,20 +4,24 @@ import type { AtomEffect } from 'recoil'
 import i18next from 'i18next'
 
 import { getOfficials } from '../../../../api/official'
+import { idTokenAtom } from '../../../recoil'
 
 let loaded = false
 
-export const remoteOfficialsEffect: AtomEffect<Official[]> = ({ setSelf, trigger }) => {
+export const remoteOfficialsEffect: AtomEffect<Official[]> = ({ getPromise, setSelf, trigger }) => {
   if (trigger === 'get' && !loaded) {
-    loaded = true
-    getOfficials()
-      .then((officials) => {
-        const sortedOfficials = [...officials].sort((a, b) => a.name.localeCompare(b.name, i18next.language))
-        setSelf(sortedOfficials)
-      })
-      .catch((reason) => {
-        console.error(reason)
-        setSelf([])
-      })
+    getPromise(idTokenAtom).then((token) => {
+      loaded = true
+
+      getOfficials()
+        .then((officials) => {
+          const sortedOfficials = [...officials].sort((a, b) => a.name.localeCompare(b.name, i18next.language))
+          setSelf(sortedOfficials)
+        })
+        .catch((reason) => {
+          console.error(reason)
+          setSelf([])
+        })
+    })
   }
 }
