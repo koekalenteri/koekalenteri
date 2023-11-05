3cc899c7a1d20caa2d8d7659474fa65619947549
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-11-05 12:30:34 +0200

    chore: add a bunch of demo events

--- src/lambda/DemoEventsFunction/demo-events.ts
+++ src/lambda/DemoEventsFunction/demo-events.ts
@@ -1,27 +1,34 @@
-import type { JsonEvent } from '../../types'
+import type { EventState, JsonEvent } from '../../types'
 
-import { addMonths, startOfDay } from 'date-fns'
+import { addMonths, addWeeks, startOfDay } from 'date-fns'
 
-const today = startOfDay(new Date()).toISOString()
+import { i18n } from '../../i18n/lambda'
+
 const timestamp = new Date().toISOString()
-const startDate = startOfDay(addMonths(Date.now(), 1)).toISOString()
 
-const draftNOMEA: JsonEvent = {
+const twoWeeksAgo = startOfDay(addWeeks(Date.now(), -2)).toISOString()
+const oneWeekAgo = startOfDay(addWeeks(Date.now(), -1)).toISOString()
+const today = startOfDay(new Date()).toISOString()
+const oneWeekAway = startOfDay(addWeeks(Date.now(), 1)).toISOString()
+const twoWeeksAway = startOfDay(addWeeks(Date.now(), 2)).toISOString()
+const monthAway = startOfDay(addMonths(Date.now(), 1)).toISOString()
+
+const draft: JsonEvent = {
   accountNumber: '',
   classes: [],
-  cost: 0,
-  costMember: 0,
+  cost: 50,
+  costMember: 40,
   createdAt: timestamp,
   createdBy: 'cron',
-  description: '',
-  endDate: startDate,
+  description: 'Joka yö automaattisesti uudelleengeneroitava tapahtuma',
+  endDate: monthAway,
   eventType: 'NOME-A',
   id: 'demo-draft-nome-a',
-  judges: [],
-  location: '',
+  judges: [842408],
+  location: 'demo',
   modifiedAt: timestamp,
   modifiedBy: 'cron',
-  name: 'DEMO: Alustava NOME-A +1kk',
+  name: 'DEMO: Luonnos NOME-A +1kk',
   official: {
     district: 'demo',
     email: 'vastaava@example.com',
@@ -41,26 +48,59 @@ const draftNOMEA: JsonEvent = {
     id: 'demo-sihteeri',
     name: 'Demo Sihteeri',
   },
-  startDate: startDate,
+  startDate: monthAway,
   state: 'draft',
 }
 
-const draftNOMEAWithEntry: JsonEvent = {
-  ...draftNOMEA,
-  id: 'demo-draft-nome-a-entry',
-  entryStartDate: today,
-}
+const dates: Partial<JsonEvent>[] = [
+  {
+    startDate: monthAway,
+    endDate: monthAway,
+    entryStartDate: oneWeekAway,
+    entryEndDate: twoWeeksAway,
+    name: '+1kk',
+  },
+  {
+    startDate: twoWeeksAway,
+    endDate: twoWeeksAway,
+    entryStartDate: today,
+    entryEndDate: oneWeekAway,
+    name: '+2vk (ilmoaika tänään->)',
+  },
+  {
+    startDate: twoWeeksAway,
+    endDate: twoWeeksAway,
+    entryStartDate: oneWeekAgo,
+    entryEndDate: today,
+    name: '+2vk (ilmoaika ->tänään',
+  },
+  {
+    startDate: today,
+    endDate: today,
+    entryStartDate: twoWeeksAgo,
+    entryEndDate: oneWeekAgo,
+    name: 'tänään',
+  },
+]
 
-const tentativeNOMEA: JsonEvent = {
-  ...draftNOMEA,
-  id: 'demo-tentative-nome-a',
-  location: 'demo',
-}
+const states: EventState[] = ['draft', 'tentative', 'cancelled', 'confirmed']
+const types: string[] = ['NOU', 'NOME-B', 'NOWT', 'NOME-A', 'NKM']
+const t = i18n.getFixedT('fi')
 
-const tentativeNOMEAWithEntry: JsonEvent = {
-  ...draftNOMEAWithEntry,
-  id: 'demo-tentative-nome-a-entry',
-  location: 'demo',
-}
+export const events: JsonEvent[] = []
 
-export const events = [draftNOMEA, draftNOMEAWithEntry, tentativeNOMEA, tentativeNOMEAWithEntry]
+for (const date of dates) {
+  for (const state of states) {
+    const stateName = t(`event.states.${state}`)
+    for (const eventType of types) {
+      events.push({
+        ...draft,
+        id: `demo-${eventType}-${state}-${date.name}`,
+        ...date,
+        eventType,
+        state,
+        name: `DEMO: ${eventType} ${date.name} [${stateName}]`,
+      })
+    }
+  }
+}
