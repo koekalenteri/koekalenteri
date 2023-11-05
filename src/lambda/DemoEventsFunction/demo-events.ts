daa4fe03fc9de9d555152a72fada93a35aefda3c
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-11-05 10:45:54 +0200

    feat: add another demo event

---
+++ src/lambda/DemoEventsFunction/demo-events.ts
@@ -0,0 +1,54 @@
+import type { JsonEvent } from '../../types'
+
+import { addMonths, startOfDay } from 'date-fns'
+
+const today = startOfDay(new Date()).toISOString()
+const timestamp = new Date().toISOString()
+const startDate = startOfDay(addMonths(Date.now(), 1)).toISOString()
+
+const draftNOMEA: JsonEvent = {
+  accountNumber: '',
+  classes: [],
+  cost: 0,
+  costMember: 0,
+  createdAt: timestamp,
+  createdBy: 'cron',
+  description: '',
+  endDate: startDate,
+  eventType: 'NOME-A',
+  id: 'demo-draft-nome-a',
+  judges: [],
+  location: '',
+  modifiedAt: timestamp,
+  modifiedBy: 'cron',
+  name: 'DEMO: Alustava NOME-A +1kk',
+  official: {
+    district: 'demo',
+    email: 'vastaava@example.com',
+    eventTypes: ['NOMA-A'],
+    id: 0,
+    name: 'Demo Koevastaava',
+  },
+  organizer: {
+    id: '400830',
+    name: 'SUOMEN NOUTAJAKOIRAJÄRJESTÖ R.Y.',
+  },
+  paymentDetails: '',
+  places: 0,
+  referenceNumber: '',
+  secretary: {
+    email: 'sihteeri@example.com',
+    id: 'demo-sihteeri',
+    name: 'Demo Sihteeri',
+  },
+  startDate: startDate,
+  state: 'draft',
+}
+
+const draftNOMEAWithEntry: JsonEvent = {
+  ...draftNOMEA,
+  id: 'demo-draft-nome-a-entry',
+  entryStartDate: today,
+}
+
+export const events = [draftNOMEA, draftNOMEAWithEntry]
--- src/lambda/DemoEventsFunction/handler.ts
+++ src/lambda/DemoEventsFunction/handler.ts
@@ -1,18 +1,16 @@
 import type { MetricsLogger } from 'aws-embedded-metrics'
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import type { AWSError } from 'aws-sdk'
-import type { JsonEvent } from '../../types'
 
 import { metricScope } from 'aws-embedded-metrics'
-import { addMonths, startOfDay } from 'date-fns'
 
-import { CONFIG } from '../config'
 import CustomDynamoClient from '../utils/CustomDynamoClient'
 import { metricsError, metricsSuccess } from '../utils/metrics'
 import { response } from '../utils/response'
 
+import { events } from './demo-events'
+
 const dynamoDB = new CustomDynamoClient()
-const { registrationTable } = CONFIG
 
 const demoEvents = metricScope(
   (metrics: MetricsLogger) =>
@@ -22,52 +20,10 @@ const demoEvents = metricScope(
       }
 
       try {
-        const timestamp = new Date().toISOString()
-        const startDate = startOfDay(addMonths(Date.now(), 1)).toISOString()
-
-        const item: JsonEvent = {
-          accountNumber: '',
-          classes: [],
-          cost: 0,
-          costMember: 0,
-          createdAt: timestamp,
-          createdBy: 'cron',
-          description: '',
-          endDate: startDate,
-          eventType: 'NOME-A',
-          id: 'demo-draft-nome-a',
-          judges: [],
-          location: '',
-          modifiedAt: timestamp,
-          modifiedBy: 'cron',
-          name: 'DEMO: Alustava NOME-A +1kk',
-          official: {
-            district: 'demo',
-            email: 'vastaava@example.com',
-            eventTypes: ['NOMA-A'],
-            id: 0,
-            name: 'Demo Koevastaava',
-          },
-          organizer: {
-            id: '400830',
-            name: 'SUOMEN NOUTAJAKOIRAJÄRJESTÖ R.Y.',
-          },
-          paymentDetails: '',
-          places: 0,
-          referenceNumber: '',
-          secretary: {
-            email: 'sihteeri@example.com',
-            id: 'demo-sihteeri',
-            name: 'Demo Sihteeri',
-          },
-          startDate: startDate,
-          state: 'draft',
-        }
-
-        await dynamoDB.write(item)
+        await dynamoDB.batchWrite(events)
 
         metricsSuccess(metrics, event.requestContext, 'demoEvents')
-        return response(200, item, event)
+        return response(200, 'ok', event)
       } catch (err) {
         console.error(err)
         metricsError(metrics, event.requestContext, 'demoEvents')
