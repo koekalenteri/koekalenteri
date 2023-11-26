7a6e4c53a938aa5e98ae381596e1d3f4220834c8
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-11-26 20:08:43 +0200

    test: add yet another level of flushes

--- src/test-utils/utils.tsx
+++ src/test-utils/utils.tsx
@@ -43,7 +43,7 @@ export function DataMemoryRouter({
 }
 
 export const flushPromises = async () => {
-  for (let i = 0; i <= 6; i++) {
+  for (let i = 0; i <= 7; i++) {
     await act(async () => {
       jest.runOnlyPendingTimers()
       await new Promise(jest.requireActual('timers').setImmediate)
