66526359f481cedd371830a8645ae440cb57bf67
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-12-28 10:17:39 +0200

    chore: add timeout and 401 handling to http requests

--- src/api/http.ts
+++ src/api/http.ts
@@ -2,7 +2,7 @@ import { enqueueSnackbar } from 'notistack'
 
 import { rum } from '../lib/client/rum'
 import { parseJSON } from '../lib/utils'
-import { API_BASE_URL } from '../routeConfig'
+import { API_BASE_URL, Path } from '../routeConfig'
 
 export class APIError extends Error {
   status: number
@@ -20,9 +20,30 @@ export class APIError extends Error {
   }
 }
 
-async function http<T>(path: string, init: RequestInit): Promise<T> {
+function anySignal(signals: AbortSignal[]): AbortSignal {
+  const controller = new AbortController()
+
+  for (const signal of signals) {
+    if (signal.aborted) {
+      controller.abort(signal.reason)
+      break
+    }
+
+    signal.addEventListener('abort', () => controller.abort(signal.reason), {
+      signal: controller.signal,
+    })
+  }
+
+  return controller.signal
+}
+
+async function httpWithTimeout<T>(path: string, init: RequestInit): Promise<T> {
   const url = API_BASE_URL + path
-  // console.debug(`fetch: ${url}`, init)
+
+  const controller = new AbortController()
+  const timer = setTimeout(() => controller.abort('timeout'), 10000)
+  const signal = init.signal ? anySignal([controller.signal, init.signal]) : controller.signal
+
   try {
     const response = await fetch(url, {
       ...init,
@@ -30,7 +51,11 @@ async function http<T>(path: string, init: RequestInit): Promise<T> {
         ...init.headers,
         Accept: 'application/json',
       },
+      signal,
     })
+
+    clearTimeout(timer)
+
     const text = await response.text()
     if (!response.ok) {
       let json = text
@@ -46,10 +71,31 @@ async function http<T>(path: string, init: RequestInit): Promise<T> {
     const parsed = parseJSON(text)
     return parsed
   } catch (err) {
+    clearTimeout(timer)
+
+    if (controller.signal.aborted) {
+      throw new APIError(new Response(null, { status: 408, statusText: 'timeout' }), {})
+    }
+
+    throw err
+  }
+}
+
+async function http<T>(path: string, init: RequestInit): Promise<T> {
+  try {
+    const result = await httpWithTimeout<T>(path, init)
+
+    return result
+  } catch (err) {
     rum()?.recordError(err)
+
     if (!(err instanceof APIError)) {
       enqueueSnackbar(`${err}`, { variant: 'error' })
+    } else if (err.status === 401) {
+      // move to homepage when authorization fails
+      window.location.replace(Path.home)
     }
+
     console.error(err)
     throw err
   }
