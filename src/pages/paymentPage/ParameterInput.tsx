b2575fdae4c22acea6deced1c9135626defe1210
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-01-09 15:43:50 +0200

    fix: checkout-timestamp format (KOE-669)

--- src/pages/paymentPage/ParameterInput.tsx
+++ src/pages/paymentPage/ParameterInput.tsx
@@ -1,6 +1,8 @@
 interface Props {
   readonly name: string
-  readonly value: string
+  readonly value: string | Date
 }
 
-export const ParameterInput = ({ name, value }: Props) => <input type="hidden" name={name} value={value} />
+export const ParameterInput = ({ name, value }: Props) => (
+  <input type="hidden" name={name} value={typeof value === 'object' ? value.toISOString() : value} />
+)
