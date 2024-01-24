7a5a6df7decfd0e975075b00c0502e2af9c1d36a
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-01-24 22:08:54 +0200

    chore: add comment to cachePolicy_UNSTABLE

--- src/pages/recoil/user/selectors.ts
+++ src/pages/recoil/user/selectors.ts
@@ -17,6 +17,19 @@ export const userSelector = selector({
 
     return null
   },
+  /**
+   * Defines the behavior of the internal selector cache. Can be useful to control the memory footprint in apps that
+   * have selectors with many changing dependencies.
+   *  @property {'lru'|'keep-all'|'most-recent'} eviction - can be set to lru (which requires that a maxSize is set), keep-all (default), or most-recent.
+   *    An lru cache will evict the least-recently-used value from the selector cache when the size of the cache
+   *    exceeds maxSize. A keep-all policy will mean all selector dependencies and their values will be indefinitely
+   *    stored in the selector cache. A most-recent policy will use a cache of size 1 and will retain only the most
+   *    recently saved set of dependencies and their values.
+   *  * Note the cache stores the values of the selector based on a key containing all dependencies and their values.
+   *    This means the size of the internal selector cache depends on both the size of the selector values as well as
+   *    the number of unique values of all dependencies.
+   *  * Note the default eviction policy (currently keep-all) may change in the future.
+   **/
   cachePolicy_UNSTABLE: {
     eviction: 'most-recent',
   },
