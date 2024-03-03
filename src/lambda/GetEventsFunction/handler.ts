67e00df9d2cc1e242535cc0ec46061d8e196a2fd
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-03-03 15:31:15 +0200

    fix: filter out draft events from public endpoint

--- src/lambda/GetEventsFunction/handler.ts
+++ src/lambda/GetEventsFunction/handler.ts
@@ -18,7 +18,7 @@ const getEventsHandler = metricScope(
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
       try {
         const items = await dynamoDB.readAll<JsonDogEvent>()
-        const publicItems = items?.map((item) => sanitizeDogEvent(item))
+        const publicItems = items?.filter((item) => item.state !== 'draft').map((item) => sanitizeDogEvent(item))
 
         metricsSuccess(metrics, event.requestContext, 'getEvents')
         return response(200, publicItems, event)
