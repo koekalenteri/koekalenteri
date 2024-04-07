c3398b982b1240504f3a089e2a87fda5d27e4a94
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-04-07 14:02:57 +0300

    fix: PaymentPage loader when already paid / registration not found (KOE-738)

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -8,6 +8,7 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import { useRecoilValue } from 'recoil'
 
+import { APIError } from '../api/http'
 import { createPayment } from '../api/payment'
 import { Path } from '../routeConfig'
 
@@ -16,19 +17,39 @@ import { ProviderButton } from './paymentPage/ProviderButton'
 import { LoadingPage } from './LoadingPage'
 import { confirmedEventSelector, registrationSelector } from './recoil'
 
-export const paymentLoader = async ({ params }: { params: Params<string> }) =>
-  params.id && params.registrationId ? defer({ response: createPayment(params.id, params.registrationId) }) : {}
+export const paymentLoader = async ({ params }: { params: Params<string> }) => {
+  const createPaymentWrap = async () => {
+    if (params.id && params.registrationId) {
+      try {
+        const result = await createPayment(params.id, params.registrationId)
+
+        return result ? result : null
+      } catch (err) {
+        // eat 404
+        if (err instanceof APIError && err.status === 404) return null
+
+        throw err
+      }
+    }
+    return {}
+  }
+  return defer({ response: createPaymentWrap() })
+}
 
 interface Props {
   readonly id?: string
+  readonly registrationId?: string
   readonly event?: PublicDogEvent | null
   readonly registration?: Registration | null
   readonly response?: CreatePaymentResponse
 }
 
-const PaymentPageWithData = ({ id, event, registration, response }: Props) => {
-  if (!event || !registration) {
+const PaymentPageWithData = ({ id, registrationId, event, registration, response }: Props) => {
+  if (!event) {
     return <>Tapahtumaa {id} ei löydy.</>
+  }
+  if (!registration) {
+    return <>Ilmoittautumista {registrationId} ei löydy.</>
   }
 
   if (registration.paymentStatus === 'SUCCESS') {
@@ -68,12 +89,20 @@ export const PaymentPage = () => {
   const { id, registrationId } = useParams()
   const event = useRecoilValue(confirmedEventSelector(id))
   const registration = useRecoilValue(registrationSelector(`${id ?? ''}:${registrationId ?? ''}`))
-  const data = useLoaderData() as { response: CreatePaymentResponse }
+  const data = useLoaderData() as { response: Promise<CreatePaymentResponse | undefined> }
 
   return (
     <Suspense fallback={<LoadingPage />}>
       <Await resolve={data.response} errorElement={<ErrorInfo />}>
-        {(response) => <PaymentPageWithData id={id} event={event} registration={registration} response={response} />}
+        {(response) => (
+          <PaymentPageWithData
+            id={id}
+            registrationId={registrationId}
+            event={event}
+            registration={registration}
+            response={response}
+          />
+        )}
       </Await>
     </Suspense>
   )
