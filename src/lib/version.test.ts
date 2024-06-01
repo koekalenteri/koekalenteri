c24040fa7334f0e4af8eaa77a5972fa180f0d7e0
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-06-01 11:57:49 +0300

    feat: version comparison utility

---
+++ src/lib/version.test.ts
@@ -0,0 +1,31 @@
+import { isEarlierVersionThan } from './version'
+
+describe('version', () => {
+  describe('isEarlierVersionThan', () => {
+    it('should return false anything earlier than 1.1.1', () => {
+      expect(isEarlierVersionThan('1.1.1')).toEqual(false)
+      expect(isEarlierVersionThan('1.1.0')).toEqual(false)
+      expect(isEarlierVersionThan('1.1')).toEqual(false)
+      expect(isEarlierVersionThan('1.0')).toEqual(false)
+      expect(isEarlierVersionThan('1')).toEqual(false)
+    })
+    it.each`
+      compare          | actual           | result
+      ${'1.0.0'}       | ${'1.0.0'}       | ${false}
+      ${'1.9.1-beta'}  | ${'1.9.1'}       | ${false}
+      ${'1.9.1-beta'}  | ${'1.9.1-beta'}  | ${false}
+      ${'1.9.1-alpha'} | ${'1.9.1-beta'}  | ${false}
+      ${'1.0.1'}       | ${'1.0.0'}       | ${true}
+      ${'1.1.2'}       | ${'1.1.1'}       | ${true}
+      ${'1.1'}         | ${'1.0.0'}       | ${true}
+      ${'1.10'}        | ${'1.9.0'}       | ${true}
+      ${'1.10'}        | ${'1.9.100'}     | ${true}
+      ${'1.10.100'}    | ${'1.9.0'}       | ${true}
+      ${'1.9.0'}       | ${'1.9.0-beta'}  | ${true}
+      ${'1.9.1'}       | ${'1.9.0-beta'}  | ${true}
+      ${'1.1.2-beta'}  | ${'1.1.2-alpha'} | ${true}
+    `('should return $result when comparing $compare to $actual', ({ compare, actual, result }) => {
+      expect(isEarlierVersionThan(compare, actual)).toEqual(result)
+    })
+  })
+})
---
+++ src/lib/version.ts
@@ -0,0 +1,23 @@
+import pkg from '../../package.json'
+
+export const appVersion = pkg.version
+
+export const isEarlierVersionThan = (version: string, current: string = appVersion) => {
+  const currentVersionParts = current.split('.')
+  const comparedVersionParts = version.split('.')
+
+  for (let i = 0; i < comparedVersionParts.length; i++) {
+    const current = currentVersionParts[i].split('-')
+    const compared = comparedVersionParts[i].split('-')
+
+    // current includes '-beta' for example, but compared does not
+    if (current.length > compared.length) return true
+    // current does not include '-beta' for example, but current does not
+    if (current.length < compared.length) return false
+    // absolute number is smaller
+    if (parseInt(current[0], 10) < parseInt(compared[0], 10)) return true
+    // '-alpha' vs '-beta' etc, in aplhabetical order
+    if (compared.length > 1 && current[1].localeCompare(compared[1]) < 0) return true
+  }
+  return false
+}
