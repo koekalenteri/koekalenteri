dda5a17eed3fe75bc32c715989ba6dc565d3d61e
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-06-01 15:07:20 +0300

    chore: move jest (=test environment) detection to env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
-export const isDevEnv = () => process.env.NODE_ENV === 'development'
+export const isDevEnv = () => process.env.NODE_ENV === 'development' && typeof jest === 'undefined'
 
-export const isTestEnv = () => process.env.NODE_ENV === 'test'
+export const isTestEnv = () => process.env.NODE_ENV === 'test' || typeof jest !== 'undefined'
 
 export const isProdEnv = () => process.env.NODE_ENV === 'production'
--- src/pages/recoil/effects/log.ts
+++ src/pages/recoil/effects/log.ts
@@ -4,7 +4,7 @@ import { isDevEnv } from '../../../lib/env'
 
 export const logEffect: AtomEffect<any> = ({ node, onSet }) => {
   onSet((newValue, oldValue, reset) => {
-    if (typeof jest === 'undefined' && isDevEnv()) {
+    if (isDevEnv()) {
       // No logs during tests
       console.debug('recoil', node.key, { newValue, oldValue, reset })
     }
