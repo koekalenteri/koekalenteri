da41f8ce7a0545d5cd41822d1ff63891fa015343
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-06-27 22:42:33 +0300

    feat: split js to chunks

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,9 @@
 import type { RouteObject } from 'react-router-dom'
 
+import { lazy } from 'react'
 import { Navigate } from 'react-router-dom'
 
 import { isDevEnv } from './lib/env'
-import AdminHomePage from './pages/admin/AdminHomePage'
-import AdminStartListPage from './pages/admin/AdminStartListPage'
-import EmailTemplateListPage from './pages/admin/EmailTemplateListPage'
-import EventCreatePage from './pages/admin/EventCreatePage'
-import EventEditPage from './pages/admin/EventEditPage'
-import EventListPage from './pages/admin/EventListPage'
-import EventTypeListPage from './pages/admin/EventTypeListPage'
-import EventViewPage from './pages/admin/EventViewPage'
-import JudgeListPage from './pages/admin/JudgeListPage'
-import OfficialListPage from './pages/admin/OfficialListPage'
-import OrganizerListPage from './pages/admin/OrganizerListPage'
-import UsersPage from './pages/admin/UsersPage'
 import { ErrorPage } from './pages/ErrorPage'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
@@ -30,21 +19,19 @@ import { SupportPage } from './pages/SupportPage'
 import { TermsPage } from './pages/TermsPage'
 import { Path } from './routeConfig'
 
-/* TBI: does not work in production build (probably due to @aws-amplify/ui-react)
 // lazy load admin section
-const AdminHomePage = lazy(() => import('./pages/admin/AdminHomePage'))
-const AdminStartListPage = lazy(() => import('./pages/admin/AdminStartListPage'))
-const EmailTemplateListPage = lazy(() => import('./pages/admin/EmailTemplateListPage'))
-const EventCreatePage = lazy(() => import('./pages/admin/EventCreatePage'))
-const EventEditPage = lazy(() => import('./pages/admin/EventEditPage'))
-const EventListPage = lazy(() => import('./pages/admin/EventListPage'))
-const EventTypeListPage = lazy(() => import('./pages/admin/EventTypeListPage'))
-const EventViewPage = lazy(() => import('./pages/admin/EventViewPage'))
-const JudgeListPage = lazy(() => import('./pages/admin/JudgeListPage'))
-const OfficialListPage = lazy(() => import('./pages/admin/OfficialListPage'))
-const OrganizerListPage = lazy(() => import('./pages/admin/OrganizerListPage'))
-const UsersPage = lazy(() => import('./pages/admin/UsersPage'))
-*/
+const AdminHomePage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/AdminHomePage'))
+const AdminStartListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/AdminStartListPage'))
+const EmailTemplateListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EmailTemplateListPage'))
+const EventCreatePage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EventCreatePage'))
+const EventEditPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EventEditPage'))
+const EventListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EventListPage'))
+const EventTypeListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EventTypeListPage'))
+const EventViewPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/EventViewPage'))
+const JudgeListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/JudgeListPage'))
+const OfficialListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/OfficialListPage'))
+const OrganizerListPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/OrganizerListPage'))
+const UsersPage = lazy(() => import(/* webpackChunkName: "admin" */ './pages/admin/UsersPage'))
 
 const routes: RouteObject[] = [
   {
