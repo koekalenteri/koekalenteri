3e05130548c570702e3512409deac3e96e05cbc0
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-07-06 13:15:00 +0300

    feat: lazy loading banner with srcset and blurred placeholder

--- src/pages/components/Banner.test.tsx
+++ src/pages/components/Banner.test.tsx
@@ -7,13 +7,10 @@ describe('Banner', () => {
     const { container } = render(<Banner />)
     expect(container).toMatchSnapshot()
     expect(container.firstChild).toHaveStyle({
-      backgroundImage: 'url(banner.webp)',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundOrigin: 'padding-box',
-      backgroundPositionY: 'calc(46px - 3vw)',
+      position: 'relative',
       width: '100%',
       height: BANNER_HEIGHT,
+      backgroundColor: '#a0a690',
     })
   })
 })
