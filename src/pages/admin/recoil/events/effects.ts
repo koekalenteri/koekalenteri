63d380ee1ba7cef873f76513108fb54b9d8cd8f9
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-07-23 21:39:09 +0300

    fix: getting admin events

--- src/pages/admin/recoil/events/effects.ts
+++ src/pages/admin/recoil/events/effects.ts
@@ -6,17 +6,20 @@ import { DefaultValue } from 'recoil'
 import { getAdminEvents } from '../../../../api/event'
 import { idTokenAtom } from '../../../recoil'
 
-import { adminEventsFetchedAtom } from './atoms'
-
 const byStartDate = (a: DogEvent, b: DogEvent) => a.startDate.valueOf() - b.startDate.valueOf()
 const sortEvents = (events: DogEvent[]): DogEvent[] => [...events].sort(byStartDate)
 
 export const adminRemoteEventsEffect: AtomEffect<DogEvent[]> = ({ getPromise, setSelf, trigger }) => {
   if (trigger === 'get') {
     setSelf(
+      getPromise(idTokenAtom).then((token) =>
+        token ? getAdminEvents(token).then((events) => sortEvents(events)) : new DefaultValue()
+      )
+      /*
       Promise.all([getPromise(idTokenAtom), getPromise(adminEventsFetchedAtom)]).then(([token, lastModified]) =>
         token ? getAdminEvents(token, lastModified).then((events) => sortEvents(events)) : new DefaultValue()
       )
+      */
     )
   }
 }
