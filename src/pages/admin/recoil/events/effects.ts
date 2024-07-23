462e6194c24eb5f00c5660bdff901a7a6fced6f9
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-07-23 09:50:05 +0300

    feat: get admin events modified after a date

--- src/pages/admin/recoil/events/effects.ts
+++ src/pages/admin/recoil/events/effects.ts
@@ -6,12 +6,18 @@ import { DefaultValue } from 'recoil'
 import { getAdminEvents } from '../../../../api/event'
 import { idTokenAtom } from '../../../recoil'
 
+import { adminEventsFetchedAtom } from './atoms'
+
 export const adminRemoteEventsEffect: AtomEffect<DogEvent[]> = ({ getPromise, setSelf, trigger }) => {
   if (trigger === 'get') {
     setSelf(
       getPromise(idTokenAtom).then((token) =>
         token
-          ? getAdminEvents(token).then((events) => events.sort((a, b) => a.startDate.valueOf() - b.startDate.valueOf()))
+          ? getPromise(adminEventsFetchedAtom).then((lastModified) =>
+              getAdminEvents(token, lastModified).then((events) =>
+                events.sort((a, b) => a.startDate.valueOf() - b.startDate.valueOf())
+              )
+            )
           : new DefaultValue()
       )
     )
