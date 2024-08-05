47e17d57372c0c44e5b2237a4279069fe2e9fea7
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-08-05 11:33:30 +0300

    refactor: reduce availableResults complexity

--- src/pages/components/registrationForm/qualifyingResultsInfo/utils.ts
+++ src/pages/components/registrationForm/qualifyingResultsInfo/utils.ts
@@ -37,6 +37,21 @@ export function availableTypes(requirements?: EventResultRequirementsByDate, eve
   return unique(requirements.rules.flatMap((rule) => asArray(rule).map((opt) => opt.type)))
 }
 
+const availableSMResults = (type?: string, eventType?: string, hasKVA?: boolean) => {
+  switch (eventType) {
+    case 'NOME-B SM':
+      return [hasKVA ? undefined : 'FI KVA-B', 'VOI1', 'VOI2', 'VOI3'].filter(Boolean)
+    case 'NOWT SM':
+      return [hasKVA ? undefined : 'FI KVA-WT', 'VOI1', 'VOI2', 'VOI3'].filter(Boolean)
+    case 'NOME-A SM':
+      if (type === 'NOME-A')
+        return [hasKVA ? undefined : 'FI KVA-FT', 'A1 CERT', 'A1 RES-CERT', 'A1', 'A2', 'A3'].filter(Boolean)
+
+      if (type === 'NOME-A KV') return ['EXC CACIT', 'EXC RES-CACIT', 'EXC', 'VG', 'G']
+  }
+  return []
+}
+
 export function availableResults(
   requirements?: EventResultRequirementsByDate,
   type?: string,
@@ -44,17 +59,10 @@ export function availableResults(
   existingResults?: ManualTestResult[]
 ) {
   if (!requirements || typeof requirements.rules === 'function') {
-    const hasKVA = existingResults?.some((r) => r.result.startsWith('FI KVA'))
-    switch (eventType) {
-      case 'NOME-B SM':
-        return [hasKVA ? undefined : 'FI KVA-B', 'VOI1', 'VOI2', 'VOI3'].filter(Boolean)
-      case 'NOWT SM':
-        return [hasKVA ? undefined : 'FI KVA-WT', 'VOI1', 'VOI2', 'VOI3'].filter(Boolean)
-      case 'NOME-A SM':
-        if (type === 'NOME-A')
-          return [hasKVA ? undefined : 'FI KVA-FT', 'A1 CERT', 'A1 RES-CERT', 'A1', 'A2', 'A3'].filter(Boolean)
+    if (eventType?.includes(' SM')) {
+      const hasKVA = existingResults?.some((r) => r.result.startsWith('FI KVA'))
 
-        if (type === 'NOME-A KV') return ['EXC CACIT', 'EXC RES-CACIT', 'EXC', 'VG', 'G']
+      return availableSMResults(type, eventType, hasKVA)
     }
     return []
   }
