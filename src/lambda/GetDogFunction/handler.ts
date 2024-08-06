56f0c5a7547598e68cbab6f5c2dd98cd7c998301
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-08-06 08:33:21 +0300

    refactor(sonar): split getDogHandler for easier cognitive load

--- src/lambda/GetDogFunction/handler.ts
+++ src/lambda/GetDogFunction/handler.ts
@@ -26,6 +26,82 @@ const GENDER: Record<string, 'F' | 'M'> = {
   hane: 'M',
 }
 
+const readDogResultsFromKlapi = async (klapi: KLAPI, regNo: string): Promise<JsonTestResult[]> => {
+  // Luetaan koetulokset käyttäen palautettua rekisterinumeroa.
+  // Jos koiralla on useampi rekkari, niin palautettu on se mille tulokset on kirjattu.
+  const apiResult = await klapi.lueKoiranKoetulokset({ Rekisterinumero: regNo, Kieli: KLKieli.Suomi })
+
+  if (apiResult.status !== 200) {
+    console.error('lueKoiranKoetulokset failed: ', JSON.stringify(apiResult))
+
+    return []
+  }
+
+  const results: JsonTestResult[] = []
+
+  for (const result of apiResult.json || []) {
+    const notes = result.lisämerkinnät.toLocaleLowerCase().trim()
+    const resCert = /vara[ -]sert/.test(notes)
+    const cert = !resCert && /sert/.test(notes)
+    const resCacit = /vara[ -]]cacit/.test(notes)
+    const cacit = !resCacit && /cacit/.test(notes)
+
+    results.push({
+      type: result.koemuoto,
+      subType: result.tapahtumanTyyppi,
+      class: result.luokka,
+      date: result.aika,
+      result: result.tulos,
+      judge: result.tuomari,
+      location: result.paikkakunta,
+
+      ext: result.tarkenne,
+      notes: result.lisämerkinnät,
+      points: result.pisteet,
+      rank: result.sijoitus,
+
+      cert,
+      resCert,
+      cacit,
+      resCacit,
+    })
+  }
+
+  return results
+}
+
+const readDogFromKlapi = async (regNo: string, existing?: JsonDog) => {
+  const klapi = new KLAPI(getKLAPIConfig)
+  const { status, json, error } = await klapi.lueKoiranPerustiedot({
+    Rekisterinumero: regNo,
+    Kieli: KLKieli.Suomi,
+  })
+
+  let dog = existing
+
+  if (status === 200 && json?.rekisterinumero) {
+    // Cache
+    dog = {
+      ...existing, // keep refined info on refres
+      kcId: json.id,
+      regNo: json.rekisterinumero,
+      name: json.nimi,
+      rfid: json.tunnistusmerkintä,
+      breedCode: json.rotukoodi as BreedCode,
+      dob: json.syntymäaika,
+      gender: GENDER[json.sukupuoli],
+      titles: json.tittelit,
+      refreshDate: new Date().toISOString(),
+    }
+
+    dog.results = await readDogResultsFromKlapi(klapi, dog.regNo)
+  } else {
+    console.error('lueKoiranPerustiedot failed: ', status, json, error)
+  }
+
+  return { dog, status, error }
+}
+
 const getDogHandler = metricScope(
   (metrics: MetricsLogger) =>
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -33,84 +109,30 @@ const getDogHandler = metricScope(
         const regNo = unescape(event.pathParameters?.regNo?.replace('~', '/') ?? '')
 
         let item = await dynamoDB.read<JsonDog>({ regNo })
-        const itemAge = item?.refreshDate ? differenceInMinutes(new Date(), new Date(item.refreshDate)) : 0
 
+        const itemAge = item?.refreshDate ? differenceInMinutes(new Date(), new Date(item.refreshDate)) : 0
         const refresh = (event.queryStringParameters && 'refresh' in event.queryStringParameters) || itemAge > 60
 
         console.log('cached: ' + JSON.stringify(item))
         console.log(`itemAge: ${itemAge}, refresh: ${refresh}`)
 
         if (!item || refresh) {
-          const klapi = new KLAPI(getKLAPIConfig)
-          const { status, json, error } = await klapi.lueKoiranPerustiedot({
-            Rekisterinumero: regNo,
-            Kieli: KLKieli.Suomi,
-          })
+          const { dog, status, error } = await readDogFromKlapi(regNo, item)
 
-          if (status === 200 && json?.rekisterinumero) {
-            // Cache
-            item = {
-              ...item, // keep refined info on refres
-              kcId: json.id,
-              regNo: json.rekisterinumero,
-              name: json.nimi,
-              rfid: json.tunnistusmerkintä,
-              breedCode: json.rotukoodi as BreedCode,
-              dob: json.syntymäaika,
-              gender: GENDER[json.sukupuoli],
-              titles: json.tittelit,
-              refreshDate: new Date().toISOString(),
-            }
-
-            // Luetaan koetulokset käyttäen palautettua rekisterinumeroa.
-            // Jos koiralla on useampi rekkari, niin palautettu on se mille tulokset on kirjattu.
-            const results = await klapi.lueKoiranKoetulokset({ Rekisterinumero: item.regNo, Kieli: KLKieli.Suomi })
-            if (results.status === 200) {
-              const res: JsonTestResult[] = []
-              for (const result of results.json || []) {
-                const notes = result.lisämerkinnät.toLocaleLowerCase().trim()
-                const resCert = /vara[ -]sert/.test(notes)
-                const cert = !resCert && /sert/.test(notes)
-                const resCacit = /vara[ -]]cacit/.test(notes)
-                const cacit = !resCacit && /cacit/.test(notes)
-                res.push({
-                  type: result.koemuoto,
-                  subType: result.tapahtumanTyyppi,
-                  class: result.luokka,
-                  date: result.aika,
-                  result: result.tulos,
-                  judge: result.tuomari,
-                  location: result.paikkakunta,
-
-                  ext: result.tarkenne,
-                  notes: result.lisämerkinnät,
-                  points: result.pisteet,
-                  rank: result.sijoitus,
-
-                  cert,
-                  resCert,
-                  cacit,
-                  resCacit,
-                })
-              }
-              item.results = res
-            } else {
-              console.error('lueKoiranKoetulokset failed: ', JSON.stringify(results))
-            }
-
-            await dynamoDB.write(item)
-          } else {
-            console.error('lueKoiranPerustiedot failed: ', status, json, error)
-            if (!item) {
-              metricsError(metrics, event.requestContext, 'getDog')
-              return response(status, 'Upstream error: ' + error, event)
-            }
-            if (status === 404 && error === 'diseased') {
-              await dynamoDB.delete({ regNo })
-              metricsError(metrics, event.requestContext, 'getDog')
-              return response(status, 'Upstream error: ' + error, event)
-            }
+          if (!dog) {
+            metricsError(metrics, event.requestContext, 'getDog')
+            return response(status, 'Upstream error: ' + error, event)
           }
+
+          if (status === 404 && error === 'diseased') {
+            await dynamoDB.delete({ regNo })
+            metricsError(metrics, event.requestContext, 'getDog')
+            return response(status, 'Upstream error: ' + error, event)
+          }
+
+          await dynamoDB.write(dog)
+
+          item = dog
         }
 
         metricsSuccess(metrics, event.requestContext, 'getDog')
