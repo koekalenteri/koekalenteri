46e9e2b7ab237999a32dc95acec2f73a9b80668a
Jukka Kurkela <jukka.kurkela@gmail.com>
2024-08-08 16:16:35 +0300

    test: refuncCancelLambda without transaction id

---
+++ src/lambda/RefundCancelFunction/refundCancelLambda.test.ts
@@ -0,0 +1,21 @@
+import { jest } from '@jest/globals'
+
+import { constructAPIGwEvent } from '../test-utils/helpers'
+
+const { default: refundCancelLambda } = await import('./handler')
+
+describe('refundCancelLambda', () => {
+  jest.spyOn(console, 'debug').mockImplementation(() => undefined)
+  const logSpy = jest.spyOn(console, 'log').mockImplementation(() => undefined)
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should return 200 when checkout-transaction-id is missing', async () => {
+    const res = await refundCancelLambda(constructAPIGwEvent('test'))
+
+    expect(res.statusCode).toEqual(200)
+    expect(logSpy).toHaveBeenCalledTimes(1)
+  })
+})
