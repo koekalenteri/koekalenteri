576f957f75d05bad874c415353cc3f2b5ca93e46
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-05-18 19:55:36 +0300

    chore(lint): disallow unused imports

--- src/pages/components/registrationForm/PaymentInfo.test.tsx
+++ src/pages/components/registrationForm/PaymentInfo.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import { RecoilRoot } from 'recoil'
-import { flushPromises } from '../../../test-utils/utils'
 import PaymentInfo from './PaymentInfo'
 
 describe('PaymentInfo', () => {
