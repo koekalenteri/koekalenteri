c25b6007ca9005bbf2bd8ab8d0b764c831431760
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-07-28 12:14:02 +0300

    fix(service-worker): verify message origin

--- src/service-worker.js
+++ src/service-worker.js
@@ -19,6 +19,8 @@ self.addEventListener('activate', (event) => {
 })
 
 self.addEventListener('message', (event) => {
+  if (event.origin !== self.location.origin) return
+
   if (event.data?.type === 'SKIP_WAITING') {
     void self.skipWaiting()
   }
